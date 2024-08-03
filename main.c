a934cc0f63292131f54b19092c008c590e9f1aa5
CobbCoding <rircobb@gmail.com>
2024-08-03 19:54:47 -0400

    implement ability to add more values

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 #include <time.h>
 
+#define NUM_STATES 3
+
 typedef struct {
-    bool *data;
+    unsigned char *data;
     size_t count;
     size_t capacity;
 } Tape;
@@ -22,15 +25,14 @@ typedef enum {
 } Direction;
     
 typedef struct {
-    bool write;
+    unsigned char expected;
+    unsigned char write;
     Direction dir;
     size_t next;
 } State;
 
 typedef struct {
-    bool expected;
-    State yes;
-    State no;
+    State value[NUM_STATES];
 } Instruction;
     
 typedef struct {
@@ -43,7 +45,7 @@ typedef struct {
 void tape_randomize(Tape *tape) {
     srand(time(NULL));
     for(size_t i = 0; i < tape->capacity; i++) {
-        tape->data[i] = rand() % 2;
+        tape->data[i] = rand() % NUM_STATES;
     }
 }
     
@@ -51,26 +53,24 @@ size_t machine_execute(Machine *machine, Instruction *inst, size_t inst_count) {
     if(machine->head >= machine->tape.capacity) {
         Tape tape = {0};
         tape.capacity = machine->tape.capacity*2;
-        tape.data = calloc(tape.capacity, sizeof(bool));
-        memcpy(tape.data, machine->tape.data, sizeof(bool)*machine->tape.capacity);
+        tape.data = calloc(tape.capacity, sizeof(*machine->tape.data));
+        memcpy(tape.data, machine->tape.data, sizeof(*machine->tape.data)*machine->tape.capacity);
         free(machine->tape.data);
         machine->tape = tape;
     }
-    
-    if(machine->tape.data[machine->head] == inst->expected) {
-        machine->tape.data[machine->head] = inst->yes.write;
-        if(machine->head == 0 && inst->yes.dir < 0) return inst_count;
-        machine->head += inst->yes.dir;
-        if(machine->head > machine->tape.count) machine->tape.count = machine->head;        
         
-        return inst->yes.next;
+    for(size_t i = 0; i < NUM_STATES; i++) {
+        if(machine->tape.data[machine->head] == inst->value[i].expected) {    
+            machine->tape.data[machine->head] = inst->value[i].write;
+            if(machine->head == 0 && inst->value[i].dir < 0) return inst_count;
+            machine->head += inst->value[i].dir;
+            if(machine->head > machine->tape.count) machine->tape.count = machine->head;        
+            
+            return inst->value[i].next;
+        }
     }
-    machine->tape.data[machine->head] = inst->no.write;    
-    if(machine->head == 0 && inst->no.dir < 0) return inst_count;    
-    machine->head += inst->no.dir;    
-    if(machine->head > machine->tape.count) machine->tape.count = machine->head;
-    
-    return inst->no.next;       
+    fprintf(stderr, "value was not in expected range\n");
+    exit(1);
 }
     
 void machine_print(Machine *machine) {
@@ -89,12 +89,12 @@ int main(int argc, char **argv) {
     if(argc == 1) {
         machine.tape.capacity = 3;
         machine.tape.count = machine.tape.capacity;        
-        machine.tape.data = malloc(sizeof(bool)*machine.tape.capacity);                
+        machine.tape.data = malloc(sizeof(unsigned char)*machine.tape.capacity);                
         tape_randomize(&machine.tape);
     } else {
         machine.tape.capacity = strlen(argv[1]);
         machine.tape.count = machine.tape.capacity;
-        machine.tape.data = malloc(sizeof(bool)*machine.tape.capacity);        
+        machine.tape.data = malloc(sizeof(unsigned char)*machine.tape.capacity);        
         for(size_t i = 0; i < machine.tape.capacity; i++) {
             machine.tape.data[i] = argv[1][i] - '0';
         }
@@ -102,10 +102,12 @@ int main(int argc, char **argv) {
     
     program.machine = &machine;            
     Instruction insts[] = {
-        // expected value, value if true, direction if true, next if true,
-        // value if false, direction if false, next if false
-        {0, {1, RIGHT, 2}, {0, RIGHT, 0}},
-        {NULL},
+        {{
+            {0, 1, RIGHT, 2},
+            {1, 0, RIGHT, 0},
+            {2, 1, RIGHT, 0},                    
+        }},
+        {{{0}}},
     };
     program.inst_count = sizeof(insts)/sizeof(*insts);
     program.insts = insts;    
@@ -114,6 +116,6 @@ int main(int argc, char **argv) {
     while(program.cur <= program.inst_count-1) {
         program.cur = machine_execute(program.machine, &program.insts[program.cur], program.inst_count);    
         machine_print(program.machine);        
-    }
+    }    
     return 0;
 }
